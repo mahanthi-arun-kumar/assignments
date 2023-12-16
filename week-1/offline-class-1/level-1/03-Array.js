58dd8529b0f007cd65b4679645c471deb636aad3
Mahanthi Arun Kumar <arun.kannu123@gmail.com>
2023-12-16 21:55:23 +0530

    check now

--- week-1/offline-class-1/level-1/03-Array.js
+++ week-1/offline-class-1/level-1/03-Array.js
@@ -1,7 +1,7 @@
 // Array handbook
 
 // Array:   push(), pop(), shift(), unshift(), splice(), slice(),
-// concat(), forEach(), map(), filter(), reduce(), find(), sort() checking1
+// concat(), forEach(), map(), filter(), reduce(), find(), sort() checking2
 
 // Run each function to see the output, play and learn by doing.
 
